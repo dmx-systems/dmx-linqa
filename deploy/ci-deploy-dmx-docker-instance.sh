3ac951288b656bd3145458b4bcde59f4f2690878
Juergen Neumann <juergen@junes.eu>
2023-07-19 13:04:25 +0200

    fix deployment

--- deploy/ci-deploy-dmx-docker-instance.sh
+++ deploy/ci-deploy-dmx-docker-instance.sh
@@ -79,15 +79,11 @@ echo "DMX_PORT=${DMX_PORT}" >>"${ENV_FILE}"
 cat "${ENV_FILE}"
 echo "dmx.websockets.url = wss://${WEB_URL}/websocket" > deploy/dmx/${TIER}-ci/conf.d/config.properties.d/10_websocket_url
 echo "dmx.host.url = https://${WEB_URL}/" > deploy/dmx/${TIER}-ci/conf.d/config.properties.d/10_host_url
-
-docker compose --env-file "${ENV_FILE}" --file deploy/docker-compose.${TIER}-ci.yaml down -v || true
-sleep 1
-#if [ $( echo "${PLUGINS}" | grep dmx-ldap ) ] || [ "${CI_PROJECT_NAME}" == "dmx-ldap" ]; then
-if [ "$( docker image ls ${CI_PROJECT_NAME}_${CI_COMMIT_REF_SLUG}-ldap | grep "${CI_PROJECT_NAME}_${CI_COMMIT_REF_SLUG}-ldap" )" ]; then
-    docker image rm ${CI_PROJECT_NAME}_${CI_COMMIT_REF_SLUG}-ldap || true
-fi
 DOCKER_IMAGE="$( docker inspect ${CI_PROJECT_NAME}-${TIER}-ldap-container | jq .[].Config.Image | sed 's/\"//g' )"
 echo "DOCKER_IMAGE=${DOCKER_IMAGE}"
+docker compose --env-file "${ENV_FILE}" --file deploy/docker-compose.${TIER}-ci.yaml down -v || true
+sleep 2
+#if [ $( echo "${PLUGINS}" | grep dmx-ldap ) ] || [ "${CI_PROJECT_NAME}" == "dmx-ldap" ]; then
 if [ "$( docker image ls | grep "${DOCKER_IMAGE}" )" ]; then
     echo "deleting old docker image ${DOCKER_IMAGE}"
     docker image rm ${DOCKER_IMAGE} || true

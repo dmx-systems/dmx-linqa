9151bef35ead27bd6a51f5c8befb9b5d2d0a0c34
Jörg Richter <jri@deepamehta.de>
2023-09-30 15:50:52 +0200

    Deploy: change create-user-account request (#10)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -7,7 +7,7 @@ import onHttpError from './error-handler'
 import messageHandler from './message-handler'
 import './element-ui'
 
-console.log('[Linqa] 2023/09/30-4')
+console.log('[Linqa] 2023/09/30-5')
 
 // 1) Init dmx library
 dmx.init({

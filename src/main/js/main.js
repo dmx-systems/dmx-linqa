9d6567f96606a29b663566655ddf631304c384a5
Jörg Richter <jri@deepamehta.de>
2023-11-24 19:29:32 +0100

    Bump dependency to DMX 5.3.4-SNAPSHOT

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -7,7 +7,7 @@ import onHttpError from './error-handler'
 import messageHandler from './message-handler'
 import './element-ui'
 
-console.log('[Linqa] 2023/11/24')
+console.log('[Linqa] 2023/11/24-2')
 
 // 1) Init dmx library
 dmx.init({

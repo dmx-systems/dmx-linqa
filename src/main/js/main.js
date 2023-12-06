5ced25163cf36fadfbde6d76fe54798b46bd163a
Jörg Richter <jri@deepamehta.de>
2023-12-06 15:20:19 +0100

    Consolidate migrations, pt.3 (#27)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -7,7 +7,7 @@ import onHttpError from './error-handler'
 import messageHandler from './message-handler'
 import './element-ui'
 
-console.log('[Linqa] 2023/12/05-2')
+console.log('[Linqa] 2023/12/06')
 
 // 1) Init dmx library
 dmx.init({

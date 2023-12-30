d18ae713a1f666c26c6cd9674e9e2f3f60746185
Jörg Richter <jri@deepamehta.de>
2023-12-30 17:25:02 -0600

    Testing: add "Twemoji Mozilla" font (#34)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -7,7 +7,7 @@ import onHttpError from './error-handler'
 import messageHandler from './message-handler'
 import './element-ui'
 
-console.log('[Linqa] 2023/12/30-4')
+console.log('[Linqa] 2023/12/30-5')
 
 // 1) Init dmx library
 dmx.init({

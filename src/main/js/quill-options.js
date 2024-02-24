f439c137e91d141b6c70fc06b35f63dc626d4a88
Jörg Richter <jri@deepamehta.de>
2024-02-24 17:03:43 -0600

    Quill editor: handle images as separate resources (#37)

--- src/main/js/quill-options.js
+++ src/main/js/quill-options.js
@@ -7,7 +7,47 @@ export default {
         [{list: 'ordered'}, {list: 'bullet'}],
         ['link', 'image', 'video'],
         [{header: [1, 2, 3, false]}]
-      ]
+      ],
+      handlers: {
+        image: selectLocalImage
+      }
     }
   }
+}
+
+function selectLocalImage() {
+  const input = document.createElement('input')
+  input.setAttribute('type', 'file')
+  input.click()
+  input.onchange = () => {
+    const file = input.files[0]     // a File object
+    console.log(file)
+    if (/^image\//.test(file.type)) {
+      saveToServer(file, this.quill)
+    } else {
+      console.warn(`${file.name} is not an image file`)
+    }
+  }
+}
+
+function saveToServer(file, editor) {
+  const fd = new FormData()
+  fd.append('image', file)
+  const xhr = new XMLHttpRequest()
+  xhr.open('POST', '/upload/' + encodeURIComponent('/'), true)    // upload to repo root dir
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      console.log('response', JSON.parse(xhr.response))
+      const url = '/filerepo/' + encodeURIComponent(JSON.parse(xhr.response).repoPath)
+      insertToEditor(url, editor)
+    } else {
+      console.warn(`Upload of ${file.name} failed`)
+    }
+  }
+  xhr.send(fd)
+}
+
+function insertToEditor(url, editor) {
+  const range = editor.getSelection()
+  editor.insertEmbed(range.index, 'image', url)
 }

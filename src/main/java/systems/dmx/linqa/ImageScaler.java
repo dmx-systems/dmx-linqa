61e3f405037539ef2f4cfcc3beee97e4e072cba0
Jörg Richter <jri@deepamehta.de>
2024-02-28 21:37:02 -0600

    Don't re-encode unscaled images (#38)

--- src/main/java/systems/dmx/linqa/ImageScaler.java
+++ src/main/java/systems/dmx/linqa/ImageScaler.java
@@ -27,42 +27,55 @@ class ImageScaler {
 
     UploadedFile scale(UploadedFile imageFile) {
         try {
+            imageFile.setBuffered();    // storing the original (unscaled) image requires re-reading the input stream
             InputStream in = imageFile.getInputStream();
-            String name = imageFile.getName();
-            String format = JavaUtils.getExtension(name);
             BufferedImage image = ImageIO.read(in);
             int width = image.getWidth();
             int height = image.getHeight();
             int max = Math.max(width, height);
-            if (max > MAX_PIXEL) {
-                float scale = MAX_PIXEL / max;
-                int targetWidth = (int) (width * scale);
-                int targetHeight = (int) (height * scale);
-                logger.info("Original " + width + "x" + height + " -> " + targetWidth + "x" + targetHeight + "(scale=" +
-                    scale + ")");
-                // FIXME: use TYPE_INT_ARGB?
-                BufferedImage scaledImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
-                Graphics2D g = scaledImage.createGraphics();
-                g.drawImage(image, 0, 0, targetWidth, targetHeight, null);     // observer=null
-                g.dispose();
-                image = scaledImage;
-            } else {
+            if (max <= MAX_PIXEL) {
                 logger.info("Size " + width + "x" + height + " (no scaling needed)");
+                in.reset();
+                return imageFile;
             }
+            float scale = MAX_PIXEL / max;
+            int targetWidth = (int) (width * scale);
+            int targetHeight = (int) (height * scale);
+            logger.info("Original " + width + "x" + height + " -> " + targetWidth + "x" + targetHeight + " (scale=" +
+                scale + ")");
+            BufferedImage scaledImage = createScaledImage(image, targetWidth, targetHeight);
+            return outputImage(scaledImage, imageFile.getName());
+        } catch (Exception e) {
+            throw new RuntimeException("ImageScaler failed", e);
+        }
+    }
+
+    // ------------------------------------------------------------------------------------------------- Private Methods
+
+    private BufferedImage createScaledImage(BufferedImage image, int width, int height) {
+        // FIXME: use TYPE_INT_ARGB?
+        BufferedImage scaledImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = scaledImage.createGraphics();
+        g.drawImage(image, 0, 0, width, height, null);     // observer=null
+        g.dispose();
+        return scaledImage;
+    }
+
+    private UploadedFile outputImage(BufferedImage image, String fileName) {
+        try {
             ByteArrayOutputStream out = new ByteArrayOutputStream() {
                 @Override
                 public synchronized byte[] toByteArray() {
                     return this.buf;
                 }
             };
+            String format = JavaUtils.getExtension(fileName);
             ImageIO.write(image, format, out);
             int size = out.size();
-            InputStream scaledIn = new ByteArrayInputStream(out.toByteArray(), 0, size);
-            return new UploadedFile(name, size, scaledIn);
+            InputStream in = new ByteArrayInputStream(out.toByteArray(), 0, size);
+            return new UploadedFile(fileName, size, in);
         } catch (Exception e) {
-            throw new RuntimeException("Storing scaled image failed", e);
+            throw new RuntimeException("Output image failed", e);
         }
     }
-
-    // ------------------------------------------------------------------------------------------------- Private Methods
 }

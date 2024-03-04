310b589b38900f5f4ac53ca38350cf7c5a2f1b7f
Jörg Richter <jri@deepamehta.de>
2024-03-03 18:59:39 -0600

    Don't store duplicate images in file repo (#39)

--- src/main/java/systems/dmx/linqa/migrations/Migration3.java
+++ src/main/java/systems/dmx/linqa/migrations/Migration3.java
@@ -19,6 +19,8 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.xml.bind.DatatypeConverter;
@@ -41,6 +43,8 @@ public class Migration3 extends Migration {
     // ---------------------------------------------------------------------------------------------- Instance Variables
 
     @Inject private FilesService files;
+
+    private Map<String, String> storedImages = new HashMap();   // base64 -> image URL (in file repo)
 
     private int imageCount = 0;
     private int nonDataUrls = 0;
@@ -106,13 +110,21 @@ public class Migration3 extends Migration {
 
     private String writeImageFile(String base64, String mimeType) {
         try {
-            String extension = mimeType.split("/")[1];
-            String fileName = String.format(IMAGE_FILE_NAME, ++imageCount, extension);
-            byte[] bytes = DatatypeConverter.parseBase64Binary(base64);
-            UploadedFile imageFile = new UploadedFile(fileName, bytes.length, new ByteArrayInputStream(bytes));
-            UploadedFile scaledImage = new ImageScaler().scale(imageFile);
-            String repoPath = files.storeFile(scaledImage, "/").getRepoPath();
-            return "/filerepo/" + JavaUtils.encodeURIComponent(repoPath);
+            String url = storedImages.get(base64);
+            if (url != null) {
+                logger.info("Duplicate already stored (" + url + ")");
+                return url;
+            } else {
+                String extension = mimeType.split("/")[1];
+                String fileName = String.format(IMAGE_FILE_NAME, ++imageCount, extension);
+                byte[] bytes = DatatypeConverter.parseBase64Binary(base64);
+                UploadedFile imageFile = new UploadedFile(fileName, bytes.length, new ByteArrayInputStream(bytes));
+                UploadedFile scaledImage = new ImageScaler().scale(imageFile);
+                String repoPath = files.storeFile(scaledImage, "/").getRepoPath();
+                url = "/filerepo/" + JavaUtils.encodeURIComponent(repoPath);
+                storedImages.put(base64, url);
+                return url;
+            }
         } catch (Exception e) {
             throw new RuntimeException("Writing image file failed", e);
         }

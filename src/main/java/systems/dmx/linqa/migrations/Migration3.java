914b83e906e5ab0f3a1268c6116727570d492a38
Jörg Richter <jri@deepamehta.de>
2024-03-04 19:01:27 -0600

    Image data-URL migration: add stats (#39)

--- src/main/java/systems/dmx/linqa/migrations/Migration3.java
+++ src/main/java/systems/dmx/linqa/migrations/Migration3.java
@@ -22,6 +22,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import javax.xml.bind.DatatypeConverter;
@@ -40,6 +41,8 @@ public class Migration3 extends Migration {
     // ------------------------------------------------------------------------------------------------------- Constants
 
     private static final String IMAGE_FILE_NAME = "zw-image-%d.%s";
+    private static final String STATS = "  %-20s - topics: %3d, images: %3d, data-URLs: %3d, duplicates: %3d -> " +
+        "%3d repo files created\n";
 
     // ---------------------------------------------------------------------------------------------- Instance Variables
 
@@ -48,7 +51,11 @@ public class Migration3 extends Migration {
     private Map<String, String> storedImages = new HashMap();   // base64 -> image URL (in file repo)
 
     private int imageCount = 0;
-    private int nonDataUrls = 0;
+
+    private Map<String, int[]> stats = new TreeMap();           // type URI -> 0: topic count
+                                                                //             1: image count
+                                                                //             2: image data URL count
+                                                                //             3: image duplicate count
 
     private Logger logger = Logger.getLogger(getClass().getName());
 
@@ -63,12 +70,21 @@ public class Migration3 extends Migration {
         transformImages(TEXTBLOCK_TEXT);
         transformImages(COMMENT);
         transformImages(COMMENT_TEXT);
+        //
+        StringBuilder sb = new StringBuilder();
+        stats.keySet().stream().forEach(typeUri -> {
+            sb.append(String.format(STATS, typeUri, stats.get(typeUri)[0], stats.get(typeUri)[1], stats.get(typeUri)[2],
+                stats.get(typeUri)[3], stats.get(typeUri)[2] - stats.get(typeUri)[3]));
+        });
+        logger.info("##### Image data-URL migration complete #####\n" + sb);
     }
 
     // ------------------------------------------------------------------------------------------------- Private Methods
 
-    private long transformImages(String typeUri) {
-        return dmx.getTopicsByType(typeUri).stream().filter(topic -> {
+    private void transformImages(String typeUri) {
+        stats.put(typeUri, new int[4]);
+        dmx.getTopicsByType(typeUri).stream().forEach(topic -> {
+            inc(typeUri, 0);
             String html = topic.getSimpleValue().toString();
             Document doc = Jsoup.parseBodyFragment(html);
             OutputSettings settings = doc.outputSettings();
@@ -76,8 +92,10 @@ public class Migration3 extends Migration {
             Elements images = doc.select("img");
             boolean hasDataUrl = false;
             for (Element image : images) {
+                inc(typeUri, 1);
                 if (transformImage(image, topic)) {
                     hasDataUrl = true;
+                    inc(typeUri, 2);
                 }
             }
             if (hasDataUrl) {
@@ -85,8 +103,7 @@ public class Migration3 extends Migration {
                 logger.info(newHtml);
                 topic.setSimpleValue(newHtml);
             }
-            return true;
-        }).count();
+        });
     }
 
     private boolean transformImage(Element image, Topic topic) {
@@ -97,7 +114,6 @@ public class Migration3 extends Migration {
         if (!src.startsWith("data:")) {
             log.append("not a data-URL");
             logger.info(log.toString());
-            nonDataUrls++;
             return false;
         }
         CharacterReader reader = new CharacterReader(src);
@@ -110,30 +126,34 @@ public class Migration3 extends Migration {
         String base64 = src.substring(reader.pos());
         log.append("mimeType=\"" + mimeType + "\", encoding=\"" + encoding + "\", size=" + base64.length());
         logger.info(log.toString());
-        String url = writeImageFile(base64, mimeType);
+        String url = writeImageFile(base64, mimeType, topic.getTypeUri());
         image.attr("src", url);
         return true;
     }
 
-    private String writeImageFile(String base64, String mimeType) {
+    private String writeImageFile(String base64, String mimeType, String typeUri) {
         try {
             String url = storedImages.get(base64);
             if (url != null) {
                 logger.info("Duplicate already stored (" + url + ")");
-                return url;
-            } else {
-                String extension = mimeType.split("/")[1];
-                String fileName = String.format(IMAGE_FILE_NAME, ++imageCount, extension);
-                byte[] bytes = DatatypeConverter.parseBase64Binary(base64);
-                UploadedFile imageFile = new UploadedFile(fileName, bytes.length, new ByteArrayInputStream(bytes));
-                UploadedFile scaledImage = new ImageScaler().scale(imageFile);
-                String repoPath = files.storeFile(scaledImage, "/").getRepoPath();
-                url = "/filerepo/" + JavaUtils.encodeURIComponent(repoPath);
-                storedImages.put(base64, url);
+                inc(typeUri, 3);
                 return url;
             }
+            String extension = mimeType.split("/")[1];
+            String fileName = String.format(IMAGE_FILE_NAME, ++imageCount, extension);
+            byte[] bytes = DatatypeConverter.parseBase64Binary(base64);
+            UploadedFile imageFile = new UploadedFile(fileName, bytes.length, new ByteArrayInputStream(bytes));
+            UploadedFile scaledImage = new ImageScaler().scale(imageFile);
+            String repoPath = files.storeFile(scaledImage, "/").getRepoPath();
+            url = "/filerepo/" + JavaUtils.encodeURIComponent(repoPath);
+            storedImages.put(base64, url);
+            return url;
         } catch (Exception e) {
             throw new RuntimeException("Writing image file failed", e);
         }
+    }
+
+    void inc(String typeUri, int item) {
+        stats.get(typeUri)[item]++;
     }
 }

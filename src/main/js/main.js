e77a718024b9e0f9c23be2b2b7ffd351ae71ef28
Jörg Richter <jri@deepamehta.de>
2024-03-10 18:06:54 -0600

    Lazy-load videos, 2nd try (#40)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import messageHandler from './message-handler'
 import './element-ui'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2024/03/09')
+console.log('[Linqa] 2024/03/10')
 
 // 1) Init dmx library
 dmx.init({

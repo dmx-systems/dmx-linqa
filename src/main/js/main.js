5cd6c6756e5e83856c9d9b65bff456b76316f845
Jörg Richter <jri@deepamehta.de>
2024-03-14 14:22:05 -0600

    Test: remove DE example mail config from /deploy (#41)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import messageHandler from './message-handler'
 import './element-ui'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2024/03/13-4')
+console.log('[Linqa] 2024/03/14')
 
 // 1) Init dmx library
 dmx.init({

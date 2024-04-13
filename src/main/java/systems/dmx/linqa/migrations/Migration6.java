cb5280518844f318e9d9e9f2c33a8e37ce9aba21
Jörg Richter <jri@deepamehta.de>
2024-04-13 15:42:47 +0200

    Rearrange Arrow/Line migrations (#48)

--- src/main/java/systems/dmx/linqa/migrations/Migration6.java
+++ src/main/java/systems/dmx/linqa/migrations/Migration6.java
@@ -1,6 +1,5 @@
 package systems.dmx.linqa.migrations;
 
-import static systems.dmx.core.Constants.*;
 import static systems.dmx.linqa.Constants.*;
 
 import systems.dmx.core.service.Migration;
@@ -10,13 +9,17 @@ import java.util.logging.Logger;
 
 
 /**
- * Creates topic type "Line" and transforms existing "Arrow" instances.
+ * Transforms existing "Arrow" instances.
  * Deletes topic type "Arrow".
  * <p>
  * Part of Linqa 1.8
- * Runs ALWAYS.
+ * Runs only in UPDATE mode.
  */
-public class Migration5 extends Migration {
+public class Migration6 extends Migration {
+
+    // ------------------------------------------------------------------------------------------------------- Constants
+
+    private static final String ARROW = "linqa.arrow";
 
     // ---------------------------------------------------------------------------------------------- Instance Variables
 
@@ -26,17 +29,12 @@ public class Migration5 extends Migration {
 
     @Override
     public void run() {
-        // 1) create topic type "Line"
-        dmx.createTopicType(
-            mf.newTopicTypeModel(LINE, "Line", ENTITY)
-                .addCompDef(mf.newCompDefModel(LINE, LOCKED, ONE))
-        );
-        // 2) transform arrows into lines
+        // 1) transform arrows into lines
         long count = dmx.getTopicsByType(ARROW).stream().filter(topic -> {
             topic.setTypeUri(LINE);
             return true;
         }).count();
-        // 3) delete topic type "Arrow"
+        // 2) delete topic type "Arrow"
         dmx.deleteTopicType(ARROW);
         logger.info("### Linqa Arrow->Line transformation complete\n  Arrows transformed: " + count);
     }

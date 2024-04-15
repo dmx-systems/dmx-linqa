5a41c31e88fca5482be41ed4615ce88dfae4b637
Jörg Richter <jri@deepamehta.de>
2024-04-16 01:18:38 +0200

    Migration: transform arrows to new format (#48)

--- src/main/java/systems/dmx/linqa/migrations/Migration6.java
+++ src/main/java/systems/dmx/linqa/migrations/Migration6.java
@@ -2,7 +2,9 @@ package systems.dmx.linqa.migrations;
 
 import static systems.dmx.linqa.Constants.*;
 
+import systems.dmx.core.service.Inject;
 import systems.dmx.core.service.Migration;
+import systems.dmx.topicmaps.TopicmapsService;
 
 import java.util.logging.Logger;
 
@@ -23,6 +25,9 @@ public class Migration6 extends Migration {
 
     // ---------------------------------------------------------------------------------------------- Instance Variables
 
+    @Inject
+    private TopicmapsService tms;
+
     private Logger logger = Logger.getLogger(getClass().getName());
 
     // -------------------------------------------------------------------------------------------------- Public Methods
@@ -32,6 +37,9 @@ public class Migration6 extends Migration {
         // 1) transform arrows into lines
         long count = dmx.getTopicsByType(ARROW).stream().filter(topic -> {
             topic.setTypeUri(LINE);
+            tms.getTopicmapTopics(topic.getId()).stream().forEach(topicmap -> {
+                topicmap.getRelatingAssoc().setProperty(ARROWHEADS, "end", false);
+            });
             return true;
         }).count();
         // 2) delete topic type "Arrow"

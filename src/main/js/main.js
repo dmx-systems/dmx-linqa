a2a4e12f6528a171569330d0343ef989a50f160a
Jörg Richter <jri@deepamehta.de>
2024-04-18 12:50:32 +0200

    Update Sign-up to 3.2.0-SNAPSHOT (#51)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import messageHandler from './message-handler'
 import './element-ui'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2024/04/17-2')
+console.log('[Linqa] 2024/04/18')
 
 // 1) Init dmx library
 dmx.init({

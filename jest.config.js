a3bf6dd9ec4117a481f0202d96181ef54ce1cba3
Robert <robert.schuster.rs01@gmail.com>
2024-04-22 23:11:25 +0200

    Add JS testing basics Relates-To: #46

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  modulePaths: ['src/main/js'],
+  moduleFileExtensions: ['js'],
+}
---
+++ src/test/js/store/admin.test.js
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+import { describe, expect, jest, test } from '@jest/globals'
+
+import adminStore from 'store/admin'
+
+describe('admin store', () => {
+
+    test('state.primaryPanel should initially be "lq-workspace-list"', () => {
+        // given:
+        //const
+    })
+})

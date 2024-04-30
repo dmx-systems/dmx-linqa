1608a6ad40c421b2023978acfba5ff8224485588
Jörg Richter <jri@deepamehta.de>
2024-04-30 21:45:14 +0200

    Internal: factor out canvas event handling into mixin (#56)

--- src/main/js/components/lq-canvas.vue
+++ src/main/js/components/lq-canvas.vue
@@ -37,16 +37,14 @@ import lq from '../lq-globals'
 
 const context = {}
 
-let APP_HEADER_HEIGHT
-
 export default {
 
   mixins: [
     require('./mixins/topicmap').default,
     require('./mixins/viewport').default,
     require('./mixins/selection').default,
+    require('./mixins/canvas-events').default,
     require('./mixins/roles').default,
-    // require('./mixins/dragging').default     // TODO: needed?
   ],
 
   provide: {
@@ -55,10 +53,6 @@ export default {
 
   created () {
     context.config = this.config
-  },
-
-  mounted () {
-    APP_HEADER_HEIGHT = document.querySelector('.lq-app-header').clientHeight
   },
 
   data () {
@@ -215,11 +209,6 @@ export default {
       return this.$store.state.isEditActive.includes(topic.id) ? 'form' : 'info'
     },
 
-    wheelZoom (e) {
-      // console.log('wheelZoom', e)
-      this.setZoom(this.zoom - .003 * e.deltaY, e.clientX, e.clientY - APP_HEADER_HEIGHT)
-    },
-
     isActionAvailable (action) {
       return action.only !== undefined ? action.only : true
     },
@@ -255,235 +244,6 @@ export default {
 
     writableTopicFilter (topic) {
       return this.config('multiEnabled', topic) && (this.isLinqaAdmin || !topic.children['linqa.locked']?.value)
-    },
-
-    transitionend () {
-      this.$store.dispatch('transitionEnd')
-    },
-
-    // 3 vue-selecto event handlers
-
-    // "Selecto" passes a custom "dragStart" event. Regarding cancelling it provides 3 functions:
-    // - stop()           -- stops entire "selecto" logic both, drag selection + deselection
-    // - preventDrag()    -- prevents drag selection but still allows deselection
-    // - preventDefault() -- alias of native event's preventDefault()?
-    onDragSelectStart (e) {
-      if (e.inputEvent.target === this.$refs.canvas) {
-        if (e.inputEvent.shiftKey) {
-          console.log('onDragSelectStart() -> STARTING DRAG SELECTION and PREVENT CANVAS PAN')
-          e.inputEvent.stopImmediatePropagation()
-        } else {
-          console.log('onDragSelectStart() -> PREVENT DRAG SELECTION (shift key not pressed)')
-          // Prevent drag selection but still allow *deselection* (click on canvas) which is also part
-          // of "selecto" logic. e.stop() on the other hand would stop the entire "selecto" logic.
-          e.preventDrag()
-        }
-      } else {
-        const target = e.inputEvent.target
-        const element = this.$refs.selecto.getSelectableElements().find(e => e.contains(target))
-        if (element) {
-          if (this.$refs.moveable.isMoveableElement(target) || this.targets.includes(element)) {
-            console.log('onDragSelectStart() -> PREVENT DRAG SELECTION (clicked on already selected item)',
-              this.$refs.moveable.isMoveableElement(target), this.targets.includes(element),
-              element !== undefined)
-            // Stop "selecto" logic (drag selection + deselection) if we've clicked
-            // 1) a multi-selection in order to drag it (for a multi-selection isMoveableElement() is true), OR
-            // 2) a selected item in order to do a text-selection drag in form mode
-            e.stop()
-          } else {
-            console.log('onDragSelectStart() -> SELECT ITEM', element.dataset.id)
-          }
-        } else {
-          console.log('onDragSelectStart() -> PREVENT ITEM SELECTION (clicked element is not selectable)')
-          e.stop()
-        }
-      }
-    },
-
-    onSelect (e) {
-      console.log('onSelect()')
-      this.$store.dispatch('updateSelection', {
-        addTopics: e.added.map(el => el.__vue__.topic),
-        removeTopicIds: e.removed.map(el => Number(el.dataset.id))
-      })
-      setTimeout(() => {    // Vue.nextTick() does not work here
-        this.positionGroupToolbar()
-      }, 100)
-    },
-
-    onSelectEnd (e) {
-      console.log('onSelectEnd()', e.isDragStart)
-      if (e.isDragStart) {
-        e.inputEvent.preventDefault()
-        setTimeout(() => {
-          this.$refs.moveable.dragStart(e.inputEvent)
-        })
-      }
-    },
-
-    // 11 vue-moveable event handlers
-
-    // "Moveable" passes a custom "dragStart" event. Regarding cancelling it provides 3 functions:
-    // - stopAble() -- stops the current "able", e.g. you can stop a "draggable" behavior but not a "pinchable" behavior
-    // - stopDrag() -- stops all "able" behaviors. stopDrag() is more radical than stopAble()
-    // - stop()     -- not in API docs, apparently an alias for stopAble()
-    onDragStart (e) {
-      const parent = e.inputEvent.target.closest('button, input, label[role="radio"], .ql-editor')
-      if (parent) {
-        console.log('onDragStart() -> PREVENT ITEM DRAG (clicked on input element)', e.target.dataset.id)
-        e.stopDrag()
-      } else {
-        console.log('onDragStart() -> STARTING ITEM DRAG', e.target.dataset.id)
-        const topic = this.findTopic(e.target)
-        if (topic) {
-          this.dragStartPos = {[topic.id]: topic.pos}
-        } else {
-          // FIXME: this should never happen. When deselecting an item or selecting a different one a
-          // superfluous start-item-drag is triggered together with intended start-canvas-pan or start-item-drag
-          // (for the previously selected item). This happens only on mobile. Timing is an issue here.
-          console.warn(`onDragStart() -> ABORT ITEM DRAG (item ${e.target.dataset.id} not in selection)`)
-          e.stopDrag()
-        }
-      }
-    },
-
-    onDrag (e) {
-      // console.log('onDrag()')
-      this.config('moveHandler')(this.findTopic(e.target), e.dist[0], e.dist[1])
-    },
-
-    onDragEnd (e) {
-      console.log('onDragEnd()')
-      this.$store.dispatch('storeTopicPos', this.findTopic(e.target))
-    },
-
-    onClickGroup (e) {
-      this.$refs.selecto.clickTarget(e.inputEvent, e.inputTarget)
-    },
-
-    onDragGroupStart (e) {
-      // console.log('onDragGroupStart()')
-      // remembers start positions
-      const p = {}
-      e.targets.forEach(el => {
-        const topic = this.findTopic(el)
-        p[topic.id] = topic.pos
-      })
-      this.dragStartPos = p
-    },
-
-    onDragGroup (e) {
-      // console.log('onDragGroup()')
-      e.targets.forEach(el => {
-        const topic = this.findTopic(el)
-        this.config('moveHandler', topic)(topic, e.dist[0], e.dist[1])
-        this.positionGroupToolbar()
-      })
-    },
-
-    onDragGroupEnd (e) {
-      // console.log('onDragGroupEnd()')
-      const topicCoords = e.targets.map(el => {
-        const topic = this.findTopic(el)
-        const pos = topic.pos
-        return {
-          topicId: topic.id,
-          x: pos.x,
-          y: pos.y
-        }
-      })
-      this.$store.dispatch('storeTopicCoords', topicCoords)
-    },
-
-    onResize (e) {
-      // Note: snap-to-grid while resize is in progress did not work as expected (the mouse is no longer over the
-      // component when width is changed programmatically?). Workaround is to snap only on resize-end.
-      const height = this.resizeStyle === 'xy' && e.height
-      this.setSize(e.target, e.width, height)
-    },
-
-    onResizeEnd ({target}) {
-      const topic = this.findTopic(target)
-      const width = lq.snapToGrid(topic.getViewProp('dmx.topicmaps.width'))
-      const height = this.resizeStyle === 'xy' && lq.snapToGrid(topic.getViewProp('dmx.topicmaps.height'))
-      this.setSize(target, width, height)
-      this.$store.dispatch('storeTopicSize', topic)
-    },
-
-    onRotate ({target, rotate}) {
-      const angle = Math.round(rotate / 5) * 5          // rotate in 5 deg steps
-      target.style.transform = `rotate(${angle}deg)`;   // view update not strictly required but improves rendering
-      this.findTopic(target).setViewProp('linqa.angle', angle)     // update model
-    },
-
-    onRotateEnd (e) {
-      this.$store.dispatch('storeTopicAngle', this.findTopic(e.target))
-    },
-
-    // 6 vue-moveable event handlers (canvas panning + pinching)
-
-    // "Moveable" passes a custom "dragStart" event. Regarding cancelling it provides 3 functions:
-    // - stopAble() -- stops the current "able", e.g. you can stop a "draggable" behavior but not a "pinchable" behavior
-    // - stopDrag() -- stops all "able" behaviors. stopDrag() is more radical than stopAble()
-    // - stop()     -- not in API docs, apparently an alias for stopAble()
-    onPanStart (e) {
-      if (e.inputEvent.target !== this.$refs.canvas) {
-        console.log('onPanStart() -> PREVENT CANVAS PAN (not clicked on canvas)', e.inputEvent.touches?.length)
-        if (e.inputEvent.touches?.length === 2) {
-          // Only stop the "draggable" (which handles canvas panning), NOT the "pinchable" (as handled by the same
-          // Moveable instance). So the current mousedown/touchstart event can still invoke a "pinch" event. Calling
-          // e.stopDrag() on the other hand would stop invocation of *all* the drag events, including "pinch".
-          e.stopAble()
-        } else {
-          // Cancel entire gesture, otherwise the search input field will not receive focus anymore (default prevented)
-          e.stopDrag()
-        }
-      } else {
-        console.log('onPanStart() -> STARTING CANVAS PAN')
-      }
-    },
-
-    onPan (e) {
-      // console.log('onPan()', e.isFirstDrag)
-      this.$store.dispatch('setViewport', {
-        pan: {
-          x: this.pan.x + e.delta[0],
-          y: this.pan.y + e.delta[1]
-        }
-      })
-      // FIXME: avoid mouse hover effects while panning over e.g. discussion panel, app header, or an open menu
-      /* if (e.isFirstDrag) {     // TODO: needed?
-        this.dragStart()
-      } */
-    },
-
-    onPanEnd (e) {
-      console.log('onPanEnd()')
-      // this.dragStop()          // TODO: needed?
-    },
-
-    onPinchStart (e) {
-      console.log('onPinchStart()', e.inputEvent.target === this.$refs.canvas, e)
-      this.startZoom = this.zoom
-    },
-
-    onPinch (e) {
-      // console.log('onPinch()' /*, e.inputEvent.scale, e */)
-      this.setZoom(this.startZoom * e.inputEvent.scale, e.clientX, e.clientY - APP_HEADER_HEIGHT)
-    },
-
-    onPinchEnd (e) {
-      console.log('onPinchEnd()')
-    },
-
-    //
-
-    onEnter () {
-      this.groupHover = true
-    },
-
-    onLeave () {
-      this.groupHover = false
     },
 
     //

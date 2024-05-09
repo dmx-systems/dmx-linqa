83ffa31398b8368cb34c36dd2777e01ccf888ef6
Jörg Richter <jri@deepamehta.de>
2024-05-09 17:52:57 +0200

    Show line styles in menu (#48)

--- src/main/js/components/lq-line-menu.vue
+++ src/main/js/components/lq-line-menu.vue
@@ -6,9 +6,9 @@
       <el-dropdown-item command="arrowheads.start"><div class="arrow start">&#767;</div></el-dropdown-item>
       <el-dropdown-item command="arrowheads.end"><div class="arrow end">&#767;</div></el-dropdown-item>
       <el-dropdown-item command="arrowheads.start-end"><div class="arrow start-end">&#845;</div></el-dropdown-item>
-      <el-dropdown-item command="line-style.none" divided><div class="style none">none</div></el-dropdown-item>
-      <el-dropdown-item command="line-style.dotted"><div class="style dotted">dotted</div></el-dropdown-item>
-      <el-dropdown-item command="line-style.dashed"><div class="style dashed">dashed</div></el-dropdown-item>
+      <el-dropdown-item command="line-style.none" divided><div class="style none"></div></el-dropdown-item>
+      <el-dropdown-item command="line-style.dotted"><div class="style dotted"></div></el-dropdown-item>
+      <el-dropdown-item command="line-style.dashed"><div class="style dashed"></div></el-dropdown-item>
     </el-dropdown-menu>
   </el-dropdown>
 </template>
@@ -38,7 +38,9 @@ export default {
 .lq-line-dropdown .el-dropdown-menu__item > div {
   position: relative;
   font-size: 48px;
-  pointer-events: none;     /* make the menu-items the hot spots */
+  width: 42px;
+  text-align: center;
+  pointer-events: none;     /* make the outer menu-items the hot spots */
 }
 
 .lq-line-dropdown .arrow.none {
@@ -56,6 +58,25 @@ export default {
 
 .lq-line-dropdown .arrow.start-end {
   top: -24px;
-  left: 28px;
+  left: 13px;
+}
+
+.lq-line-dropdown .style {
+  box-sizing: border-box;
+  height: 30px;     /* 30px is .el-dropdown-menu__item line-height */
+  top: -13px;
+  border-bottom-width: 4px;
+}
+
+.lq-line-dropdown .style.none {
+  border-bottom-style: solid;
+}
+
+.lq-line-dropdown .style.dotted {
+  border-bottom-style: dotted;
+}
+
+.lq-line-dropdown .style.dashed {
+  border-bottom-style: dashed;
 }
 </style>

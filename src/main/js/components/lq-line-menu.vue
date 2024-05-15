825d6f43fa798edc6d691e499b0672e958305f04
Jörg Richter <jri@deepamehta.de>
2024-05-15 14:58:37 +0200

    Fix: render line menu on Windows/Linux (#48)

--- src/main/js/components/lq-line-menu.vue
+++ src/main/js/components/lq-line-menu.vue
@@ -2,10 +2,18 @@
   <el-dropdown class="lq-line-menu" size="medium" trigger="click" @command="handle">
     <span ref="trigger"></span>
     <el-dropdown-menu class="lq-line-dropdown" slot="dropdown">
-      <el-dropdown-item command="arrowheads.none"><div class="arrow none">&#9135;&#9135;</div></el-dropdown-item>
-      <el-dropdown-item command="arrowheads.start"><div class="arrow start">&#767;</div></el-dropdown-item>
-      <el-dropdown-item command="arrowheads.end"><div class="arrow end">&#767;</div></el-dropdown-item>
-      <el-dropdown-item command="arrowheads.start-end"><div class="arrow start-end">&#845;</div></el-dropdown-item>
+      <el-dropdown-item command="arrowheads.none">
+        <lq-svg-arrow arrowheads="none"></lq-svg-arrow>
+      </el-dropdown-item>
+      <el-dropdown-item command="arrowheads.start">
+        <lq-svg-arrow arrowheads="start"></lq-svg-arrow>
+      </el-dropdown-item>
+      <el-dropdown-item command="arrowheads.end">
+        <lq-svg-arrow arrowheads="end"></lq-svg-arrow>
+      </el-dropdown-item>
+      <el-dropdown-item command="arrowheads.start-end">
+        <lq-svg-arrow arrowheads="start-end"></lq-svg-arrow>
+      </el-dropdown-item>
       <el-dropdown-item command="line-style.none" divided><div class="style none"></div></el-dropdown-item>
       <el-dropdown-item command="line-style.dotted"><div class="style dotted"></div></el-dropdown-item>
       <el-dropdown-item command="line-style.dashed"><div class="style dashed"></div></el-dropdown-item>
@@ -15,6 +23,7 @@
 
 <script>
 export default {
+
   methods: {
 
     handle (command) {
@@ -25,47 +34,32 @@ export default {
     open () {
       this.$refs.trigger.click()
     }
+  },
+
+  components: {
+    'lq-svg-arrow': require('./lq-svg-arrow').default
   }
 }
 </script>
 
 <style>
 /* the actual dropdown menus are body mounted */
+.lq-line-dropdown .el-dropdown-menu__item {
+  line-height: unset !important;
+}
+
 .lq-line-dropdown .el-dropdown-menu__item + .el-dropdown-menu__item {
   margin-top: 9px;
 }
 
-.lq-line-dropdown .el-dropdown-menu__item > div {
-  position: relative;
-  font-size: 48px;
-  width: 42px;
-  text-align: center;
-  pointer-events: none;     /* make the outer menu-items the hot spots */
-}
-
-.lq-line-dropdown .arrow.none {
-  top: -6px;
-}
-
-.lq-line-dropdown .arrow.start {
-  top: -12px;
-}
-
-.lq-line-dropdown .arrow.end {
-  transform: rotate(180deg);
-  top: 13px;
-}
-
-.lq-line-dropdown .arrow.start-end {
-  top: -24px;
-  left: 13px;
-}
-
 .lq-line-dropdown .style {
+  position: relative;
   box-sizing: border-box;
-  height: 30px;     /* 30px is .el-dropdown-menu__item line-height */
+  width: 50px;
+  height: 30px;             /* 30px is original .el-dropdown-menu__item line-height */
   top: -13px;
   border-bottom-width: 4px;
+  pointer-events: none;     /* make the outer menu-items the hot spots, not the top-moved divs */
 }
 
 .lq-line-dropdown .style.none {
---
+++ src/main/js/components/lq-svg-arrow.vue
@@ -0,0 +1,50 @@
+<template>
+  <svg class="lq-svg-arrow" :viewBox="viewBox">
+    <defs>
+      <marker id="arrowhead" markerWidth="5" markerHeight="4" refX="4" refY="2" orient="auto-start-reverse">
+        <polygon points="0 0, 5 2, 0 4" :fill="color" />
+      </marker>
+    </defs>
+    <line x1="0" y1="0" :x2="width" y2="0" :stroke="color" stroke-width="4" :marker-start="markerStartUrl"
+      :marker-end="markerEndUrl" />
+  </svg>
+</template>
+
+<script>
+export default {
+
+  props: {
+    arrowheads: String
+  },
+
+  data () {
+    return {
+      width: 50,          /* is lq-line-menu item width */
+      height: 30,         /* is lq-line-menu item height */
+      color: '#606266',   /* is .el-dropdown-menu__item color */
+      markerUrl: 'url(#arrowhead)'
+    }
+  },
+
+  computed: {
+
+    viewBox () {
+      return `0 ${-this.height / 2 - 2} ${this.width} ${this.height}`
+    },
+
+    markerStartUrl () {
+      return ['start', 'start-end'].includes(this.arrowheads) && this.markerUrl
+    },
+
+    markerEndUrl () {
+      return ['end', 'start-end'].includes(this.arrowheads) && this.markerUrl
+    }
+  }
+}
+</script>
+
+<style>
+.lq-svg-arrow {
+  overflow: visible;
+}
+</style>

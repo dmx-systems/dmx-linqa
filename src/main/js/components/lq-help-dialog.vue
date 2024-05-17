8dbadb061db2db3ef0660e8d52db0b4f47c1d351
Jörg Richter <jri@deepamehta.de>
2024-05-17 23:04:14 +0200

    Show help texts and slider (#62)

--- src/main/js/components/lq-help-dialog.vue
+++ src/main/js/components/lq-help-dialog.vue
@@ -1,5 +1,10 @@
 <template>
-  <el-dialog class="lq-help-dialog" :visible="visible" width="350px" @close="close">
+  <el-dialog custom-class="lq-help-dialog" :visible="visible" width="350px" @open="open" @close="close">
+    <div v-html="text"></div>
+    <div slot="footer">
+      <el-button round size="mini" icon="el-icon-arrow-left" @click="prev"></el-button>
+      <el-button round size="mini" icon="el-icon-arrow-right" @click="next"></el-button>
+    </div>
   </el-dialog>
 </template>
 
@@ -12,15 +17,44 @@ export default {
 
   data () {
     return {
+      texts: [],    // array of String
+      i: 0          // index of current text
     }
   },
 
   computed: {
+
+    text () {
+      return this.texts[this.i]
+    },
+
+    num () {
+      return this.texts.length
+    }
   },
 
   methods: {
+
+    open () {
+      if (!this.num) {
+        this.$store.dispatch('getHelpTexts').then(texts => {
+          this.texts = texts
+        })
+      }
+    },
+
     close () {
       this.$emit('close')
+    },
+
+    prev () {
+      this.i--
+      if (this.i === -1) this.i = this.num - 1
+    },
+
+    next () {
+      this.i++
+      if (this.i === this.num) this.i = 0
     }
   }
 }

0ffb9ce99217ea419255a8e45737f28251502ec2
Jörg Richter <jri@deepamehta.de>
2024-05-18 20:32:04 +0200

    Add help content (#62)

--- src/main/js/components/lq-help-dialog.vue
+++ src/main/js/components/lq-help-dialog.vue
@@ -1,6 +1,6 @@
 <template>
   <el-dialog custom-class="lq-help-dialog" :visible="visible" width="820px" @open="open" @close="close">
-    <el-carousel :autoplay="false" indicator-position="outside" trigger="click" height="360px">
+    <el-carousel :autoplay="false" indicator-position="outside" trigger="click" height="400px">
       <el-carousel-item v-for="(page, i) in pages" :key="i">
         <div :class="['page', `page-${i + 1}`, 'dmx-html-field']" v-html="page"></div>
       </el-carousel-item>
@@ -56,7 +56,7 @@ export default {
 }
 
 .lq-help-dialog .el-dialog__header {
-  padding: 0;
+  padding: 0;           /* Element UI default is "20px 20px 10px" */
 }
 
 .lq-help-dialog .el-dialog__body {

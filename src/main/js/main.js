cbbe0fe628cc8e037767baba506abacb3f9e44a3
Jörg Richter <jri@deepamehta.de>
2024-05-30 13:09:46 +0200

    Fix: note/textblock forms are gray (#58)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import messageHandler from './message-handler'
 import './element-ui'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2024/05/29-2')
+console.log('[Linqa] 2024/05/30')
 
 // 1) Init dmx library
 dmx.init({

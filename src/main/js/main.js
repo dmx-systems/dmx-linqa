4bef29f668bcbe2714e662575e83e1055872fadd
Jörg Richter <jri@deepamehta.de>
2024-07-03 19:57:39 -0600

    Temporarily change timer to trigger digests (#70)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import messageHandler from './message-handler'
 import './element-ui'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2024/07/03-2')
+console.log('[Linqa] 2024/07/03-3')
 
 // 1) Init dmx library
 dmx.init({

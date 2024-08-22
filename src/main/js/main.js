cfc049b941afc0f94c8aab55be44c3a2f4ef0e2a
Jörg Richter <jri@deepamehta.de>
2024-08-22 18:12:44 +0200

    POM: upgrade to dmx-sign-up 3.3.0-SNAPSHOT

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import messageHandler from './message-handler'
 import './element-ui'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2024/08/15-3')
+console.log('[Linqa] 2024/08/22')
 
 // 1) Init dmx library
 dmx.init({

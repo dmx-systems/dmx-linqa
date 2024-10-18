53bf7aa66451a2a359bcc869dc2360f46baf41b9
Jörg Richter <jri@deepamehta.de>
2024-10-18 14:36:45 +0200

    Fix: no canvas zoom on scroll emoji palette (#78)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import messageHandler from './message-handler'
 import './element-ui'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2024/10/17-2')
+console.log('[Linqa] 2024/10/18')
 
 // 1) Init dmx library
 dmx.init({

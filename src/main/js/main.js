c6d8caba30c6a8cc6440c6e1689a4135eda77341
Jörg Richter <jri@deepamehta.de>
2024-12-04 22:46:47 +0100

    Edit items when they have reactions (#88)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import messageHandler from './message-handler'
 import './element-ui'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2024/12/01-2')
+console.log('[Linqa] 2024/12/04')
 
 // 1) Init dmx library
 dmx.init({

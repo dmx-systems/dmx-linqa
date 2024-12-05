405af7fef7c667ef96bcd1315b5c26b315edaed0
Jörg Richter <jri@deepamehta.de>
2024-12-05 22:22:19 +0100

    Vue 3: set compile-time flags (#87)

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,10 @@ module.exports = env => {
       }),
       new VueLoaderPlugin(),
       new DefinePlugin({
-        DEV: env.WEBPACK_SERVE
+        DEV: env.WEBPACK_SERVE,
+        __VUE_OPTIONS_API__: 'true',
+        __VUE_PROD_DEVTOOLS__: 'false',
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false'
       }),
       AutoImport({
         resolvers: [ElementPlusResolver()]

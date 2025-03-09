4dc1c95ef6853ecd8058fcf6f52042b2f3d61ea2
Jörg Richter <jri@deepamehta.de>
2025-03-09 16:45:06 -0600

    Fix: multi-selection w/ new unsaved items (#100)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import app from './app'
 import './element-plus'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2025/03/08')
+console.log('[Linqa] 2025/03/09')
 
 // 1) Init DMX library
 dmx.init({

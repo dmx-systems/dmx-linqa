57a3f9520021b1e64032257680fcf66739976731
Jörg Richter <jri@deepamehta.de>
2025-03-13 15:48:26 -0600

    Help pages: update modify-item image (emoji reaction) (#89)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import app from './app'
 import './element-plus'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2025/03/12')
+console.log('[Linqa] 2025/03/13')
 
 // 1) Init DMX library
 dmx.init({

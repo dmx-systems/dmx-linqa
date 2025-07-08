1f62c5e54ff71de0f20aebd4ed68dd9f1d856fc7
Jörg Richter <jri@deepamehta.de>
2025-07-08 22:44:44 +0200

    Fix: resize discussion panel after close/reopen (#116)

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import app from './app'
 import './element-plus'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2025/07/04')
+console.log('[Linqa] 2025/07/08')
 
 // 1) Init DMX library
 dmx.init({

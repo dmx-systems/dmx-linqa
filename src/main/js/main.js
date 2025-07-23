26bd38c75840a1903a2a363d8b677e83f988c5df
Jörg Richter <jri@deepamehta.de>
2025-07-23 17:58:08 +0200

    Fix: bundle "Ubuntu Bold" (instead "Medium")
    See dmx-platform/dmx-platform#578

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import app from './app'
 import './element-plus'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2025/07/23')
+console.log('[Linqa] 2025/07/23-2')
 
 // 1) Init DMX library
 dmx.init({

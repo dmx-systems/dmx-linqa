87b85526124c8992daf1872267099130727cdc96
Jörg Richter <jri@deepamehta.de>
2026-01-31 19:47:01 +0100

    Bump dependency to dmx-api 4.0.1

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import app from './app'
 import './element-plus'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2026/01/31-2')
+console.log('[Linqa] 2026/01/31-3')
 
 // 1) Init DMX library
 dmx.init({

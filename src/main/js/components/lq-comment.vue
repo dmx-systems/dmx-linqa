3d46dc943747b8fa7d88471c07ee1610e9a4d52e
Jörg Richter <jri@deepamehta.de>
2026-02-20 23:28:14 +0100

    Fix: display email address in comments

--- src/main/js/components/lq-comment.vue
+++ src/main/js/components/lq-comment.vue
@@ -158,7 +158,7 @@ export default {
     },
 
     emailAddress () {
-      return lq.getShowEmailAddress(this.creator) && this.creator
+      return lq.getShowEmailAddress(this.creator) ? this.creator : ''
     },
 
     date () {
--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import app from './app'
 import './element-plus'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2026/02/19')
+console.log('[Linqa] 2026/02/20')
 
 // 1) Init DMX library
 dmx.init({

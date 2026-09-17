aa31341f9ccaeb6bc5fde3445fe59417afb6aa6f
Jörg Richter <jri@deepamehta.de>
2026-09-17 13:08:59 +0200

    Bump version to 2.2.1-SNAPSHOT

--- src/main/js/main.js
+++ src/main/js/main.js
@@ -8,7 +8,7 @@ import app from './app'
 import './element-plus'
 import './country-flag-polyfill'
 
-console.log('[Linqa] 2.2')
+console.log('[Linqa] 2026-09-17')
 
 // 1) Init DMX library
 dmx.init({
